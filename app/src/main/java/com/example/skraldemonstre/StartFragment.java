e63eec371703828a0cbe19a897f08882a35b7294
Michelle Karlsson <milk@itu.dk>
2021-03-03 15:33:12 +0100

    App setup

--- app/src/main/java/com/example/skraldemonstre/StartFragment.java
+++ app/src/main/java/com/example/skraldemonstre/StartFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class StartFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,17 +17,25 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_start_screen, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_fang_monstre).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(StartFragment.this)
+                        .navigate(R.id.action_StartFragment_to_FangMonstreFragment);
+            }
+        });
+
+        view.findViewById(R.id.button_galleri).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(StartFragment.this)
+                        .navigate(R.id.action_StartFragment_to_GalleriFragment);
             }
         });
     }
